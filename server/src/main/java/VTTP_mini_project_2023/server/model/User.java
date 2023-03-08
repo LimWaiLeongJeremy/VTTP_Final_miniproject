2c7b12f4e70c08b5388277289eff778f41873a32
Jeremy <106659130+LimWaiLeongJeremy@users.noreply.github.com>
2023-03-08 06:13:47 +0000

    JWT second commit

--- server/src/main/java/VTTP_mini_project_2023/server/model/User.java
+++ server/src/main/java/VTTP_mini_project_2023/server/model/User.java
@@ -26,4 +26,5 @@ public class User {
 
     // TODO
     // getter and setter
+
 }
--- server/src/main/java/VTTP_mini_project_2023/server/service /UserService.java
+++ server/src/main/java/VTTP_mini_project_2023/server/service /UserService.java
@@ -1,6 +1,8 @@
 import org.springframework.web.binb.annotation.service;
 
+import VTTP_mini_project_2023.server.model.Role;
 import VTTP_mini_project_2023.server.model.User;
+import VTTP_mini_project_2023.server.repository.RoleRepository;
 import VTTP_mini_project_2023.server.repository.UserRepository;
 
 @Service
@@ -8,8 +10,25 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private RoleRepository roleRepo;
 
     public User registerNewUser(User user) {
         return userRepo.save(user);
+    }
+
+    public void initRolesAndUser() {
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin role");
+        roleRepo.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role");
+        roleRepo.save(userRole);
+
+        User adminUser = new User();
+        adminUser
     }
 }
