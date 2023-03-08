c01651784cf1da2d58dc1792b36db2b17d57566c
Jeremy <106659130+LimWaiLeongJeremy@users.noreply.github.com>
2023-03-08 08:34:54 +0000

    JWT 3rd commit

--- server/src/main/java/VTTP_mini_project_2023/server/configuration/JwtAuthenticationEntryPoint.java
+++ server/src/main/java/VTTP_mini_project_2023/server/configuration/JwtAuthenticationEntryPoint.java
@@ -3,7 +3,7 @@ package VTTP_mini_project_2023.server.configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtAuthenticationEntryPoint implements JwtAuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     
 
 }
