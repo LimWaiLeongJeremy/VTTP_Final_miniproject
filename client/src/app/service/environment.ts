e16fd54ce0c86b7167729df33ccaeb22f4fe28ba
Jeremy <106659130+LimWaiLeongJeremy@users.noreply.github.com>
2023-04-19 12:58:45 +0800

    Delete environment.ts

--- client/src/app/service/environment.ts
+++
@@ -1,5 +0,0 @@
-export const emvironment = {
-    production: false,
-    stripe: 'pk_live_51MyFbpBEWg41wi31ZqzUSZxXHOOVf0MWAy2ynwmovihFQPtIIVhhETmSg31UYpX8PK2GFPqQeptR4ElK27PYSpQL00Den5Zn1c',
-    serverUrl: '/api',
-};
