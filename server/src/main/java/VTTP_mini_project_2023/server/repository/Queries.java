dd9b71a1e2912b8a4c5494534d44435cbb261237
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-04-22 23:23:00 +0800

    rremove testing

--- server/src/main/java/VTTP_mini_project_2023/server/repository/Queries.java
+++ server/src/main/java/VTTP_mini_project_2023/server/repository/Queries.java
@@ -13,6 +13,4 @@ public class Queries {
     // for cart
     public static String SQL_INSERT_INTO_CART = "INSERT INTO cart (item_id, quantity, user_name) VALUES (?, ?, ?);";
     public static String SQL_DELETE_CART_BY_USERNAME = "DELETE FROM cart WHERE user_name = ?;";
-    public static String SQL_SELECT_CART_BY_USERNAME = "SELECT * FROM userCartView WHERE user_name = ?;";
-    // testing
-}
+    public static String SQL_SELECT_CART_BY_USERNAME = "SELECT * FROM userCartView WHERE user_name = ?;";}
