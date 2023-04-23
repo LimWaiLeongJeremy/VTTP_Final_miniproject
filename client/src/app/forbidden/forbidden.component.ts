3010e4184f6c2b9e36ac2ba2c02482b4a84a3220
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2023-04-23 19:43:09 +0800

    format forbidden page

--- client/src/app/forbidden/forbidden.component.ts
+++ client/src/app/forbidden/forbidden.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forbidden',
   templateUrl: './forbidden.component.html',
   styleUrls: ['./forbidden.component.css'],
 })
-export class ForbiddenComponent {}
+export class ForbiddenComponent {
+  
+  constructor( private router: Router ) { }
+
+  public routeToHome() {
+    this.router.navigateByUrl('/home');
+  }
+}
